5504955b026dafe8f40520ccdde61b114e28bd59
briz777 <60566784+briz777@users.noreply.github.com>
2022-06-23 10:36:49 +0300

    Update Settings.cs
    !!!WARNING!!!
    CHECK TWICE BEFORE COMMITING
    !!!WARNING!!!
    I didn't check my changes. So, check twice before accepting. There might be errors as I'm not a C# coder

--- furMix/DialogBoxes/Settings.cs
+++ furMix/DialogBoxes/Settings.cs
@@ -9,6 +9,7 @@ namespace furMix
     public partial class Settings : Form
     {
         List<string> devs = new List<string>();
+        int scrindex;
         public Settings()
         {
             InitializeComponent();
@@ -27,7 +28,8 @@ namespace furMix
                 Screen sc = scr[i];
                 scrlist.Items.Add(string.Format("{0} - {1}", i, sc.DeviceName));
             }
-            scrlist.SelectedIndex = scr.Length - 1;
+            //scrlist.SelectedIndex = scr.Length - 1;
+            scrlist.SelectedIndexChanged += scrlist_SelectedDisplayChanged;
             if (Properties.Settings.Default.Edition == "Professional Edition" || Properties.Settings.Default.Edition == "Misha Ter Edition" || Properties.Settings.Default.Edition == "Misha Pidor Edition")
             {
                 NetLabel.Visible = true;
@@ -49,7 +51,7 @@ namespace furMix
             string[] array = (devlist.Items[devlist.SelectedIndex] as string).Split(' ');
             int devindex = Convert.ToInt32(array[0]);
             array = (scrlist.Items[scrlist.SelectedIndex] as string).Split(' ');
-            int scrindex = Convert.ToInt32(array[0]);
+            //int scrindex = Convert.ToInt32(array[0]);
             Properties.Settings.Default.PlaybackDevice = devindex;
             Properties.Settings.Default.Screen = scrindex;
             if (Properties.Settings.Default.Edition == "Professional Edition" || Properties.Settings.Default.Edition == "Misha Ter Edition" || Properties.Settings.Default.Edition == "Misha Pidor Edition")
@@ -89,6 +91,10 @@ namespace furMix
             MessageBox.Show("Now furMix will open ports for remote control through network. It will require administrator privileges.", "Ports warning", MessageBoxButtons.OK, MessageBoxIcon.Information);
             Log.LogEvent("Opening ports...");
             WebServer.OpenPorts(Properties.Settings.Default.WebPort);
+        }
+        private void scrlist_SelectedDisplayChanged(object sender, EventArgs e)
+        {
+            scrindex = scrlist.SelectedIndex;
         }
     }
 }
